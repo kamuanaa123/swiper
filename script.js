769d324f0f9b03fdcb8e9b44fc47ae31bb2ff348
Zuhra <test@gmail.com>
2023-10-11 23:19:31 +0500

    first

---
+++ script.js
@@ -0,0 +1,78 @@
+// const swiper = new Swiper('.swiper', {
+//     direction: 'horizontal',
+//     loop: true,
+//     speed: 750,
+//     spaceBetween: 20,
+
+
+//     slidesPerView: 2,
+
+//     spaceBetween: 20,
+//     slidesPerGroup: 1,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true
+//     },
+
+
+
+//     navigation: {
+//         nextEl: 'swiper-button-next',
+//         prexEl: 'swiper-button-prev',
+//     }
+// })
+
+// const swiper = new Swiper('swiper', {
+//     direction: 'horizontal',
+//     loop: true,
+//     speed: 750,
+//     spaceBetween: 20,
+//     effect: 'cube',
+
+//     slidesPerView: 2,
+
+//     spaceBetween: 20,
+//     slidesPerGroup: 1,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true
+//     },
+
+//     navigation: {
+//         nextEl: 'swiper-button-next',
+//         prevEl: 'swiper-button-prev',
+//     }
+// })
+
+// const swiper = new Swiper('.swiper', {
+//     direction: 'horizontal',
+//     loop: true,
+//     speed: 750,
+//     spaceBetween: 20,
+//     spaceBetween: 20, 
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true
+//     },
+   
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     }
+// })
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 750,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pafination',
+        clickable: true
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+})
